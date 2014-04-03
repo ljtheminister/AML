63c81a9213c2ea17f248ff6e714156d14f034e01
ljtheminister <john.c.min@gmail.com>
2014-04-03 16:04:02 -0400

    question 2 of HW

---
+++ HW4/Frobenius_norm_estimation.m
@@ -0,0 +1,17 @@
+%% set seed for random number generation
+s = RandStream('mt19937ar','Seed',1);
+RandStream.setGlobalStream(s);
+%rng default
+
+%% generate random matrix A
+n = 500;
+p = 1000;
+
+A = rand(n, p);
+%A = randi5, n, p);
+%A = randn(n, p);
+
+norm_Frobenius = norm(A, 'fro')
+
+
+
